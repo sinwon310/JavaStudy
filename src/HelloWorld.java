ff99b778226e61665371dbdc7fb9daef7d3251e9
sinwon310 <sinwon310@naver.com>
2025-03-28 16:19:23 +0900

    집에서작업

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,10 +1,12 @@
+import java.awt.SystemColor;
 
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("이클립스 첫 실행");
-		System.out.println("Hello Eclipse");
-		System.out.println("Github연동완료");
+		System.out.println("1.이클립스 첫 실행");
+		System.out.println("2.Hello Eclipse");
+		System.out.println("3.Github연동완료");
+		System.out.println("4.집에서 작업");
 	}
 
 }
