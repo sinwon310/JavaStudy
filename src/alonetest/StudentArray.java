23ed1e2bf224976a93c39dd6b9f9d340315cbd1b
sinwon310 <sinwon310@naver.com>
2025-03-28 16:45:32 +0900

    학원, 작업

---
+++ src/alonetest/StudentArray.java
@@ -0,0 +1,15 @@
+package alonetest;
+
+public class StudentArray {
+	
+	
+
+	public static void main(String[] args) {
+		
+		String[] student = new String[3];
+		
+		student[1] = new String("1001, James");
+
+	}
+
+}
